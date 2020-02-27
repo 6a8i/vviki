f6cfbd34aad90c337d16341cb6f06adb20a633aa
Gabriele Bizzon <gbizzon@hotmail.com>
2020-02-27 19:49:21 -0300

    add projeto inicial

---
+++ menu/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#nav-menu').html('<ul id=\"list-menu\" class=\"nav flex-column\"></ul>');
+    for (let i = 0; i < 7; i++) {
+        $('#list-menu').append('<li class=\"nav-item\"><a id=\"botao\" class=\"nav-link\" href=\"#\"> menu ' + i + '</a></li>');
+    };
+
+    $('.nav-link').click(function () {
+        $('#content').html("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+    });
+});
+
