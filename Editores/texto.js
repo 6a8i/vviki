608b55282c5ed652c79c2e96fe555910c850feee
Gabriele Bizzon <gbizzon@hotmail.com>
2020-03-01 22:14:00 -0300

    Adicionado atalho de teclado na textarea de adição de paragrafo. E tratado as quebras de linhas para ficar separado no mesmo paragrafo. Futuramente as quebras de linha irão dividir uma mesma entrada em mais de um parágrafo.

--- Editores/texto.js
+++ Editores/texto.js
@@ -14,10 +14,22 @@ $(document).ready(function(){
     $(function() {
         $("ul#paragraph-list").sortable();
     });
-
-    $('#paragraph-input').change(function()
-    {
-        $('<li class="paragraph-item"><p>' + $(this).val() + '</p></li>').insertBefore(this);
-        $(this).val('Type something to add a new paragraph');
+    
+    $('#paragraph-input').on('keypress',function(e) {
+        // 13 is the code for the 'enter' key.
+        if(e.shiftKey && e.which == 13) {
+            $('<li class="paragraph-item"><p>' + $(this).val().replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '<br />') + '</p></li>').insertBefore('li.paragraph-input-item');
+            $(this).val('');
+        }
     });
+
+    // the two functions below are to hide or show the grab icon
+    $('li.paragraph-item p').mouseover(function() {
+        $(this).parent().addClass('draggable');
+    });
+
+    $('li.paragraph-item p').mouseout(function() {
+        $(this).parent().removeClass('draggable');
+    });
+    // the two functions above are to hide or show the grab icon
 });
