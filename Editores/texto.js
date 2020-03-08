ce43e3a0aeb0dd5e5d5f86500662f9efad93bfc5
Gabriele Bizzon <gbizzon@hotmail.com>
2020-03-08 11:26:37 -0300

    Correções de bugs: - Icone que não aparecia para novos paragrafos. - Editor de titulo não aceitava shift+enter - Editor de paragrafo que não estava funcionando. Adicionado biblioteca richtext para edição de texto.

--- Editores/texto.js
+++ Editores/texto.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
-    $('p.title-text').dblclick(function(){
-        $(this).hide();
-        $('#title-text').show();
-        $('#title-text').focus();
-    });
-
-    $('#title-text').focusout(function(e){
-        $('p.title-text').html($(this).val());
-        $(this).hide();
-        $('p.title-text').show();
-    });
-
     $(function() {
         $( "#sortable" ).sortable({
             placeholder: {
                 element: function(currentItem) {
-                    return $('<li><div class="paragraph-drop"></div></li>')[0];
+                    return $('<li><div class="paragraph-drop"></div></li>');
                 },
                 update: function(container, p) {
                     return;
@@ -35,14 +23,52 @@ $(document).ready(function(){
             $(this).val('');
         }
     });
-
-    // the two functions below are to hide or show the grab icon
-    $('li.paragraph-item p').mouseover(function() {
-        $(this).parent().addClass('draggable');
-    });
-
-    $('li.paragraph-item p').mouseout(function() {
-        $(this).parent().removeClass('draggable');
-    });
-    // the two functions above are to hide or show the grab icon
 });
+
+
+$(document).on('dblclick', 'p.title-text', function(){
+    $(this).hide();
+    $('#title-text').show();
+    $('#title-text').focus();
+});
+
+$(document).on('focusout', '#title-text', function(e){
+    $('p.title-text').html($(this).val());
+    $(this).hide();
+    $('p.title-text').show();
+});
+
+$(document).on('keypress', '#title-text', function(e) {
+    // 13 is the code for the 'enter' key.
+    if(e.shiftKey && e.which == 13) {
+        $('p.title-text').html($(this).val());
+        $(this).hide();
+        $('p.title-text').show();
+    }
+});
+
+$(document).on('dblclick', '.paragraph-item p', function(){
+    var val = $(this).html();
+    var li = $(this).parent();
+    li.html('<textarea id="paragraph-editor">' + val + '</textarea>');
+    $('#paragraph-editor').focus;
+});
+
+$(document).on('keypress', '#paragraph-editor', function(e) {
+    // 13 is the code for the 'enter' key.
+    if(e.shiftKey && e.which == 13) {
+        var val = $(this).val();
+        var li = $(this).parent();
+        li.html('<li class="paragraph-item"><p>' + val + '</p></li>');
+    }
+});
+
+// the two functions below are to hide or show the grab icon
+$(document).on('mouseover', 'li.paragraph-item p', function() {
+    $(this).parent().addClass('draggable');
+});
+
+$(document).on('mouseout', 'li.paragraph-item p', function() {
+    $(this).parent().removeClass('draggable');
+});
+// the two functions above are to hide or show the grab icon
